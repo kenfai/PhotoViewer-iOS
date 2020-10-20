603e5e3479fcca0acc77ae3e592c93d68385a2e4
KenFai <ngkenfai@gmail.com>
2020-10-20 21:05:59 +0800

    Initial Commit

---
+++ PhotoViewer/PhotoViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoViewerApp.swift
+//  PhotoViewer
+//
+//  Created by Ginger on 20/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct PhotoViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
